c96ae3d6cb25a29caf0b0baf9e9fc2df11e18150
mirabilos <t.glaser@tarent.de>
2020-09-07 17:20:03 +0200

    switch to ecnbits_mkcmsg

--- c/server/server.c
+++ c/server/server.c
@@ -193,6 +193,8 @@ do_packet(int s)
 	char tm[21];
 	const char *trc;
 	int af;
+	void *cmsgbuf;
+	size_t cmsgsz;
 
 	io.iov_base = data;
 	io.iov_len = sizeof(data) - 1;
@@ -234,31 +236,20 @@ do_packet(int s)
 		warn("getsockname");
 		return;
 	}
-	/*
-	 * getsockname returns AF_INET6 for v4-mapped sockets, too,
-	 * which, while correct, leads to the wrong cmsg set below,
-	 * and therefore to no outgoing traffic class being set, so
-	 * avoid using those (on nōn-Linux especially)
-	 */
-#ifdef DEBUG
-	fprintf(stderr, "D: socket is %d (%s)\n",
-	    af, af == AF_INET ? "IPv4" : "IPv6");
-#endif
+	/* pre-allocate one cmsg buffer to reuse */
+	if (!(cmsgbuf = ecnbits_mkcmsg(NULL, &cmsgsz, af, 0))) {
+		warn("ecnbits_mkcmsg");
+		return;
+	}
+	mh.msg_control = cmsgbuf;
+	mh.msg_controllen = cmsgsz;
 
 	len = snprintf(data, sizeof(data), "%s %s %s %s -> 0",
 	    revlookup(mh.msg_name, mh.msg_namelen),
 	    tm, ECNBITS_DESC(ecn), trc);
 	io.iov_len = len;
 	do {
-		union {
-			unsigned char buf[CMSG_SPACE(sizeof(int))];
-			struct cmsghdr msg;
-		} cmsgbuf;
-
-		mh.msg_control = &cmsgbuf;
-		mh.msg_controllen = sizeof(cmsgbuf);
-		memset(mh.msg_control, 0, sizeof(cmsgbuf));
-		fill_tc_cmsg(CMSG_FIRSTHDR(&mh), af,
+		ecnbits_mkcmsg(cmsgbuf, &cmsgsz, af,
 		    data[len - 1] - '0');
 
 		sendmsg(s, &mh, 0);
