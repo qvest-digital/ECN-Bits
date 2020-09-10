ee678eb2a1e78836a003badfec71bd25e45070ad
mirabilos <t.glaser@tarent.de>
2020-09-10 03:41:15 +0200

    these replacement functions will be needed
    other than that, only poll, recvmsg and sendmsg are missing

---
+++ c/util/rpl_err.h
@@ -0,0 +1,7 @@
+__declspec(noreturn)
+extern err(int, const char *, ...);
+__declspec(noreturn)
+extern errx(int, const char *, ...);
+
+extern warn(const char *, ...);
+extern warnx(const char *, ...);
