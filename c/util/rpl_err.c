438460688bb3b76189369d75ad4d62082224ff99
mirabilos <t.glaser@tarent.de>
2020-09-10 20:56:40 +0200

    stub {err,warn}{,x}(3) for now and getprogname on WinAPI

---
+++ c/util/rpl_err.c
@@ -0,0 +1,102 @@
+/*-
+ * Copyright © 2020
+ *	mirabilos <t.glaser@tarent.de>
+ * Copyright © 2016, 2017
+ *	mirabilos <m@mirbsd.org>
+ * Licensor: Deutsche Telekom
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "rpl_err.h"
+
+#ifdef _WIN32
+static LPWSTR rplerr_getprogname(void);
+#define RPLERR_PROGNAME	rplerr_getprogname()
+#define RPLERR_PROGFMT	"%S"	/* would be %ls in POSIX but… */
+#else
+extern const char *__progname;
+#define RPLERR_PROGNAME	__progname
+#define RPLERR_PROGFMT	"%s"
+#endif
+
+void
+err(int eval, const char *fmt, ...)
+{
+	fprintf(stderr, RPLERR_PROGFMT ": %s: %s",
+	    RPLERR_PROGNAME, fmt, /* stub */ __func__);
+	exit(eval);
+}
+
+void
+errx(int eval, const char *fmt, ...)
+{
+	fprintf(stderr, RPLERR_PROGFMT ": %s: %s",
+	    RPLERR_PROGNAME, fmt, /* stub */ __func__);
+	exit(eval);
+}
+
+void
+warn(const char *fmt, ...)
+{
+	fprintf(stderr, RPLERR_PROGFMT ": %s: %s",
+	    RPLERR_PROGNAME, fmt, /* stub */ __func__);
+}
+
+void
+warnx(const char *fmt, ...)
+{
+	fprintf(stderr, RPLERR_PROGFMT ": %s: %s",
+	    RPLERR_PROGNAME, fmt, /* stub */ __func__);
+}
+
+#ifdef _WIN32
+static LPWSTR
+rplerr_getprogname(void)
+{
+	static LPWSTR progname = NULL;
+
+	if (!progname) {
+		LPWSTR buf = NULL;
+		DWORD len = 0;
+		do {
+			LPWSTR cp;
+			DWORD n;
+
+			len += MAX_PATH;
+			if (!(cp = realloc(buf, len * sizeof(buf[0])))) {
+				free(buf);
+				return (L"(out of memory)");
+			}
+			buf = cp;
+			if ((n = GetModuleFileNameW(NULL, buf, len)) == 0) {
+				free(buf);
+				return (L"(cannot get program name)");
+			}
+		} while (n >= len);
+		progname = buf;
+	}
+	return (progname);
+}
+#endif
--- c/util/rpl_err.h
+++ c/util/rpl_err.h
@@ -1,7 +1,37 @@
-__declspec(noreturn)
-extern err(int, const char *, ...);
-__declspec(noreturn)
-extern errx(int, const char *, ...);
+/*-
+ * Copyright © 2020
+ *	mirabilos <t.glaser@tarent.de>
+ * Licensor: Deutsche Telekom
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
 
-extern warn(const char *, ...);
-extern warnx(const char *, ...);
+#ifndef RPL_ERR_H
+#define RPL_ERR_H
+
+#ifdef _MSC_VER
+__declspec(noreturn)
+#endif
+extern void err(int, const char *, ...);
+#ifdef _MSC_VER
+__declspec(noreturn)
+#endif
+extern void errx(int, const char *, ...);
+
+extern void warn(const char *, ...);
+extern void warnx(const char *, ...);
+
+#endif
