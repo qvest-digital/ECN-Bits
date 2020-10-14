beaec80a5f8745e18371fff1cf59dec79dfa9f50
mirabilos <t.glaser@tarent.de>
2020-10-15 01:34:49 +0200

    attempt at an OpenJDK PoC… failing due to JPMS/Jigsaw of all things
    https://stackoverflow.com/q/64362855/2171120
    
    https://stackoverflow.com/q/28439229/2171120 has someone else
    trying similar things…

---
+++ jdk/src/main/java/de/telekom/llcto/ecn_bits/jdk/client/Main.java
@@ -0,0 +1,226 @@
+package de.telekom.llcto.ecn_bits.jdk.client;
+
+/*-
+ * Copyright © 2020
+ *      mirabilos <t.glaser@tarent.de>
+ * Licensor: Deutsche Telekom
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
+
+import de.telekom.llcto.ecn_bits.jdk.lib.Bits;
+import de.telekom.llcto.ecn_bits.jdk.lib.ECNStatistics;
+import org.evolvis.tartools.rfc822.FQDN;
+import org.evolvis.tartools.rfc822.IPAddress;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.ECNBitsDatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Example ECN-Bits client program
+ *
+ * @author mirabilos (t.glaser@tarent.de)
+ */
+public final class Main {
+    private static class IPorFQDN {
+        final boolean resolved;
+        final String s;
+        final InetAddress[] a = new InetAddress[1];
+
+        IPorFQDN(final String i) {
+            resolved = false;
+            s = i;
+        }
+
+        IPorFQDN(final String i, final InetAddress ip) {
+            resolved = true;
+            s = i;
+            a[0] = ip;
+        }
+    }
+
+    private static RuntimeException usage(final String err) {
+        if (err != null) {
+            System.err.println("E: " + err);
+        }
+        System.err.println("Usage: java -jar target/client-*-cli.jar hostname port [tc]");
+        System.exit(1);
+        return new RuntimeException();
+    }
+
+    private static RuntimeException die(final String err) {
+        System.err.println("E: " + err);
+        System.exit(1);
+        return new RuntimeException();
+    }
+
+    private static IPorFQDN parseHostname(final String s) {
+        if ("".equals(s)) {
+            throw usage("empty hostname");
+        }
+        if (FQDN.isDomain(s)) {
+            return new IPorFQDN(s);
+        }
+        final InetAddress i6 = IPAddress.v6(s);
+        if (i6 != null) {
+            return new IPorFQDN(s, i6);
+        }
+        final InetAddress i4 = IPAddress.v4(s);
+        if (i4 != null) {
+            return new IPorFQDN(s, i4);
+        }
+        throw usage("invalid hostname: " + s);
+    }
+
+    public static void main(String[] argv) {
+        final Bits outBits;
+        final byte outTc;
+        if (argv.length == 3) {
+            if ("NO".equals(argv[2])) {
+                outBits = Bits.NO;
+                outTc = outBits.getBits();
+            } else if ("ECT0".equals(argv[2])) {
+                outBits = Bits.ECT0;
+                outTc = outBits.getBits();
+            } else if ("ECT1".equals(argv[2])) {
+                outBits = Bits.ECT1;
+                outTc = outBits.getBits();
+            } else if ("CE".equals(argv[2])) {
+                outBits = Bits.CE;
+                outTc = outBits.getBits();
+            } else {
+                try {
+                    final Integer v = Integer.decode(argv[2]);
+                    if (v == null || v < 0 || v > 255) {
+                        throw usage("invalid traffic class: " + argv[2]);
+                    }
+                    outBits = null;
+                    outTc = (byte) (v & 0xFF);
+                } catch (NumberFormatException e) {
+                    throw usage("invalid traffic class number: " + argv[2] + ": " + e);
+                }
+            }
+        } else if (argv.length == 2) {
+            outBits = Bits.ECT0;
+            outTc = outBits.getBits();
+        } else {
+            throw usage(null);
+        }
+
+        final IPorFQDN hostname = parseHostname(argv[0]);
+
+        if ("".equals(argv[1])) {
+            throw usage("empty port");
+        }
+        final int port;
+        try {
+            port = Integer.parseUnsignedInt(argv[1]);
+        } catch (NumberFormatException e) {
+            throw usage("bad port: " + argv[1] + ": " + e);
+        }
+        if (port < 1) {
+            throw usage("port too small: " + port);
+        }
+        if (port > 65535) {
+            throw usage("port too large: " + port);
+        }
+
+        final ECNBitsDatagramSocket sock;
+        try {
+            sock = new ECNBitsDatagramSocket();
+        } catch (SocketException e) {
+            throw die("could not create socket: " + e);
+        }
+        sock.startMeasurement();
+
+        System.out.printf("connect to [%s]:%d with %s%n", hostname.resolved ?
+            hostname.a[0].getHostAddress() : hostname.s, port,
+          outBits == null ? String.format("0x%02X", outTc) : outBits.getShortname());
+        boolean oneSuccess = false;
+        try {
+            sock.setSoTimeout(1000);
+            sock.setTrafficClass(outTc);
+            final byte[] buf = new byte[512];
+            final InetAddress[] dstArr = hostname.resolved ? hostname.a :
+              InetAddress.getAllByName(hostname.s);
+            for (final InetAddress dst : dstArr) {
+                System.out.printf(" → [%s]:%d%n",
+                  dst.getHostAddress(), port);
+                buf[0] = 'h';
+                buf[1] = 'i';
+                buf[2] = '!';
+                final DatagramPacket psend = new DatagramPacket(buf, 3, dst, port);
+                try {
+                    sock.send(psend);
+                } catch (IOException e) {
+                    System.out.println("!! send: " + e);
+                    continue;
+                }
+                final DatagramPacket precv = new DatagramPacket(buf, buf.length);
+                while (true) {
+                    try {
+                        sock.receive(precv);
+                    } catch (SocketTimeoutException e) {
+                        break;
+                    } catch (IOException e) {
+                        System.out.println("!! recv: " + e);
+                        break;
+                    }
+                    final String stamp = ZonedDateTime.now(ZoneOffset.UTC)
+                      .truncatedTo(ChronoUnit.MILLIS)
+                      .format(DateTimeFormatter.ISO_INSTANT);
+                    final Byte trafficClass = sock.retrieveLastTrafficClass();
+                    oneSuccess = true;
+                    final String userData = new String(buf, StandardCharsets.UTF_8);
+                    System.out.printf("• %s %s%n%s%n", stamp, Bits.print(trafficClass), userData.trim());
+                }
+            }
+            if (oneSuccess) {
+                System.out.println(" ‣ Success!");
+            } else {
+                System.out.println("!! failed !!");
+            }
+        } catch (UnknownHostException e) {
+            System.out.println("!! resolve: " + e);
+        } catch (SocketException e) {
+            System.out.println("!! setsockopt: " + e);
+        } finally {
+            try {
+                final ECNStatistics stats = sock.getMeasurement(false);
+                System.out.println(stats == null ?
+                  "!! no congestion measurement" :
+                  String.format("ℹ %.2f%% of %d packets received over %d ms were congested",
+                    stats.getCongestionFactor() * 100.0, stats.getReceivedPackets(),
+                    stats.getLengthOfMeasuringPeriod() / 1000000L));
+            } catch (ArithmeticException e) {
+                System.out.println("!! ECNStatistics: " + e);
+            }
+            if (!sock.isClosed()) {
+                sock.close();
+            }
+        }
+    }
+}
---
+++ jdk/src/main/java/de/telekom/llcto/ecn_bits/jdk/lib/ECNStatistics.java
@@ -0,0 +1,122 @@
+package de.telekom.llcto.ecn_bits.jdk.lib;
+
+/*-
+ * Copyright © 2020
+ *      mirabilos <t.glaser@tarent.de>
+ * Licensor: Deutsche Telekom
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
+
+/**
+ * Statistics about ECN CE bits set on received traffic in the last interval
+ *
+ * @author mirabilos (t.glaser@tarent.de)
+ */
+@SuppressWarnings("unused")
+public class ECNStatistics {
+    /**
+     * Offset between {@link System#currentTimeMillis()} and
+     * {@link System#nanoTime()} — add to nanoTime() to get
+     * nanoseconds since the epoch.
+     *
+     * The year 2106 in nanoseconds since 1970 fits into 62 bits.
+     */
+    private static final long NANO_OFFSET;
+
+    static {
+        // this initialisation code is *carefully* tweaked for precision
+        final long curMS = System.currentTimeMillis();
+        final long curNS = System.nanoTime();
+        NANO_OFFSET = curMS * 1000000L - curNS;
+    }
+
+    private final long nanoStart;
+    private final long nanoEnd;
+    private final int congested;
+    private final int packets;
+
+    /**
+     * Private constructor for an ECN-Bits statistics measurement structure
+     *
+     * @param start       timestamp
+     * @param measurement of packets
+     */
+    public ECNStatistics(final long start, final int[] measurement) {
+        nanoEnd = System.nanoTime();
+        nanoStart = start;
+        congested = measurement[1];
+        packets = measurement[0] + measurement[1];
+    }
+
+    /**
+     * Returns the timestamp of the start of the measuring period
+     *
+     * @return nanoseconds since the epoch (1970)
+     */
+    public long getStartOfMeasuringPeriod() {
+        return NANO_OFFSET + nanoStart;
+    }
+
+    /**
+     * Returns the timestamp of the end of the measuring period
+     *
+     * @return nanoseconds since the epoch (1970)
+     */
+    public long getEndOfMeasuringPeriod() {
+        return NANO_OFFSET + nanoEnd;
+    }
+
+    /**
+     * Returns the length of the measuring period
+     *
+     * @return nanoseconds since the starting timestamp
+     */
+    public long getLengthOfMeasuringPeriod() {
+        return nanoEnd - nanoStart;
+    }
+
+    /**
+     * Returns the number of packets which had the ECN CE mark
+     * that were received during the measuring period
+     *
+     * @return number of congested packets
+     */
+    public int getCongestedPackets() {
+        return congested;
+    }
+
+    /**
+     * Returns the total number of packets
+     * that were received during the measuring period
+     *
+     * @return total number of packets received
+     */
+    public int getReceivedPackets() {
+        return packets;
+    }
+
+    /**
+     * Returns the congestion factor, that is,
+     * how many packets received during the measuring period
+     * had the ECN CE mark
+     *
+     * @return 0 ≤ factor ≤ 1
+     */
+    public double getCongestionFactor() {
+        return (double) congested / (double) packets;
+    }
+}
