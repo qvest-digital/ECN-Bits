98d38faf8dd66ec98d26195bc04734983f67d14a
mirabilos <t.glaser@tarent.de>
2020-12-15 22:16:16 +0100

    add temporary test to visually verify exception IP/port printing
    (test passed including scope IDs)

--- android/ecn-lib/src/androidTest/java/de/telekom/llcto/ecn_bits/android/lib/JNIInstrumentedTest.java
+++ android/ecn-lib/src/androidTest/java/de/telekom/llcto/ecn_bits/android/lib/JNIInstrumentedTest.java
@@ -28,6 +28,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 import static de.telekom.llcto.ecn_bits.android.lib.Testable.assertThrows;
 import static org.junit.Assert.*;
@@ -118,5 +122,53 @@ public class JNIInstrumentedTest {
         JNI.n_getsockname(fd, ap);
         Log.i("ECN-Bits-JNITest", " bound to " + ap.get().toString());
         JNI.n_close(fd);
+    }
+
+    @Test
+    public void testTmp() throws UnknownHostException {
+        InetSocketAddress isa;
+        InetAddress ia;
+        Throwable t;
+        int nt = 0;
+        String st;
+        Log.i("ECN-Bits-JNITest", "tmp {{{");
+
+        ia = Inet6Address.getByName("::");
+        isa = new InetSocketAddress(ia, ++nt);
+        st = String.format("t%d", nt);
+        final InetSocketAddress finalIsa = isa;
+        t = assertThrows(IOException.class,
+          () -> JNI.n_bind(-1, JNI.AddrPort.addr(finalIsa), finalIsa.getPort(), JNI.AddrPort.scopeId(finalIsa)),
+          st);
+        Log.i("ECN-Bits-JNITest", String.format("t%d: <%s> -> %s", nt, isa, t.getMessage()));
+
+        ia = InetAddress.getByName("0.0.0.0");
+        isa = new InetSocketAddress(ia, ++nt);
+        st = String.format("t%d", nt);
+        final InetSocketAddress finalIsa1 = isa;
+        t = assertThrows(IOException.class,
+          () -> JNI.n_bind(-1, JNI.AddrPort.addr(finalIsa1), finalIsa1.getPort(), JNI.AddrPort.scopeId(finalIsa1)),
+          st);
+        Log.i("ECN-Bits-JNITest", String.format("t%d: <%s> -> %s", nt, isa, t.getMessage()));
+
+        ia = InetAddress.getByName("127.0.0.1");
+        isa = new InetSocketAddress(ia, ++nt);
+        st = String.format("t%d", nt);
+        final InetSocketAddress finalIsa2 = isa;
+        t = assertThrows(IOException.class,
+          () -> JNI.n_bind(-1, JNI.AddrPort.addr(finalIsa2), finalIsa2.getPort(), JNI.AddrPort.scopeId(finalIsa2)),
+          st);
+        Log.i("ECN-Bits-JNITest", String.format("t%d: <%s> -> %s", nt, isa, t.getMessage()));
+
+        ia = InetAddress.getByName("ff02::1%wlan0");
+        isa = new InetSocketAddress(ia, ++nt);
+        st = String.format("t%d", nt);
+        final InetSocketAddress finalIsa3 = isa;
+        t = assertThrows(IOException.class,
+          () -> JNI.n_bind(-1, JNI.AddrPort.addr(finalIsa3), finalIsa3.getPort(), JNI.AddrPort.scopeId(finalIsa3)),
+          st);
+        Log.i("ECN-Bits-JNITest", String.format("t%d: <%s> -> %s", nt, isa, t.getMessage()));
+
+        Log.i("ECN-Bits-JNITest", "tmp }}}");
     }
 }
