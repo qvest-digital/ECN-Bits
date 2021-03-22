15f77561ecbdbda6931c5541f6b280a1a55eec59
mirabilos <t.glaser@tarent.de>
2021-03-23 00:10:19 +0100

    document multiple ways to try to fix this for Mono…
    … which *ALL* fail on *only* Mono…

--- dotnet/unmngd.cs
+++ dotnet/unmngd.cs
@@ -10,7 +10,12 @@ public class API {
 
 		rv = Unmanaged.ecnbits_example_ndo();
 		if (rv >= 2) {
+			// this will show 97 on Mono but clear the last error too…
+			//System.Console.WriteLine(Marshal.GetLastWin32Error());
+			// and _this_ fails on Mono because it needs Winsock codes, dotnet/Linux works, Windows works…
+			//throw new SocketException(Marshal.GetLastWin32Error());
 			// this will use WSAGetLastError() to retrieve the error code
+			// (except on Mono…)
 			throw new SocketException();
 		}
 		return rv;
