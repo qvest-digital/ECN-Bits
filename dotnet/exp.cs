18dead1b2ce0b6e2d5c50e9795229750eed97076
mirabilos <t.glaser@tarent.de>
2021-03-22 23:56:18 +0100

    write some environment info to the console for more debugging

--- dotnet/exp.cs
+++ dotnet/exp.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Net.Sockets;
+// for RuntimeInformation
+using System.Runtime.InteropServices;
 
 namespace ECNBits.DotNet.Experiment {
 
 public class Exp {
 	public static void Main(string[] args) {
+		Console.WriteLine(Environment.Is64BitProcess);
+		Console.WriteLine(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+		    "Windows" : RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ?
+		    "GNU/Linux" : "other");
+		Console.WriteLine(RuntimeInformation.ProcessArchitecture);
 		var se = API.Do();
 		Console.WriteLine((int)se.SocketErrorCode);
 		Console.WriteLine(se.ErrorCode);
