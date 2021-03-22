393678d9852a729c44be55912dbba00b21eeaf71
mirabilos <t.glaser@tarent.de>
2021-03-22 23:56:30 +0100

    actually do call the unmanaged DLL’s C code to transform to an…
    … exception… except (hah!) it outputs nōnsensical values, on all
    platforms; need to investigate.

--- dotnet/exp.cs
+++ dotnet/exp.cs
@@ -12,11 +12,16 @@ public class Exp {
 		    "Windows" : RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ?
 		    "GNU/Linux" : "other");
 		Console.WriteLine(RuntimeInformation.ProcessArchitecture);
-		var se = API.Do();
-		Console.WriteLine((int)se.SocketErrorCode);
-		Console.WriteLine(se.ErrorCode);
-		Console.WriteLine(se.NativeErrorCode);
-		Console.WriteLine(se);
+		try {
+			var i = API.Do();
+			Console.WriteLine("ok:");
+			Console.WriteLine(i);
+		} catch (SocketException se) {
+			Console.WriteLine((int)se.SocketErrorCode);
+			Console.WriteLine(se.ErrorCode);
+			Console.WriteLine(se.NativeErrorCode);
+			Console.WriteLine(se);
+		}
 	}
 }
 
--- dotnet/unmngd.cs
+++ dotnet/unmngd.cs
@@ -1,22 +1,31 @@
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 namespace ECNBits.DotNet.Experiment {
 
 public class API {
-	public static SocketException Do() {
-		try {
-			Unmanaged.ThrowUp();
-			return null;
-		} catch (SocketException se) {
-			return se;
+	#region wrappers
+	public static int Do() {
+		int rv;
+
+		rv = Unmanaged.ecnbits_example_ndo();
+		if (rv >= 2) {
+			// this will use WSAGetLastError() to retrieve the error code
+			throw new SocketException();
 		}
+		return rv;
 	}
+	#endregion
 }
 
 internal class Unmanaged {
-	internal static int ThrowUp() {
-		throw new SocketException((int) SocketError.ConnectionRefused);
-	}
+	#region native
+	private const string LIB = "cLib";
+
+	[DllImport(LIB, CallingConvention=CallingConvention.Cdecl)]
+	internal static extern int ecnbits_example_ndo();
+
+	#endregion
 }
 
 }
