31c4ce99fb30db234a598a65e76f8cd73a40353a
mirabilos <t.glaser@tarent.de>
2021-03-24 21:53:19 +0100

    [WIP] build Win DLL for all architectures and load the correct one
    using the assembly source path’s arch-specific subdirectory,
    amending the LD_LIBRARY_PATH equivalent at runtime before the
    DLL is even loaded
    
    • ARM missing (I probablt need another Visual Studio component)
    • untested (should not change anything) on GNU/Linux, etc.

--- dotnet/unmngd.cs
+++ dotnet/unmngd.cs
@@ -19,12 +19,43 @@ public class API {
 }
 
 internal class Unmanaged {
+	#region windllpath
+	[DllImport("kernel32", CharSet=CharSet.Unicode, SetLastError=true)]
+	private static extern bool SetDllDirectory(string path);
+
+	static Unmanaged() {
+		if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			return;
+		string arch;
+		string mydir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+		switch (RuntimeInformation.ProcessArchitecture) {
+		case Architecture.X86:
+			arch = "i386";
+			break;
+		case Architecture.X64:
+			arch = "amd64";
+			break;
+		case Architecture.Arm:
+			arch = "armhf";
+			break;
+		case Architecture.Arm64:
+			arch = "arm64";
+			break;
+		default:
+			System.Console.Write("ECNBits: unknown process architecture: ");
+			System.Console.WriteLine(RuntimeInformation.ProcessArchitecture);
+			return;
+		}
+		if (!SetDllDirectory(System.IO.Path.Combine(mydir, arch)))
+			throw new System.ComponentModel.Win32Exception();
+	}
+	#endregion
+
 	#region native
 	internal const string LIB = "cLib";
 
 	[DllImport(LIB, CallingConvention=CallingConvention.Cdecl, SetLastError=true)]
 	internal static extern int ecnbits_example_ndo();
-
 	#endregion
 }
 
